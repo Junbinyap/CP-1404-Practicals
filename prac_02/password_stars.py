d27ef0f94c9178bf14c1cc3166735b7b6019a083
Junbin <junbin.yap@my.jcu.edu.au>
2025-02-09 20:44:41 +0800

    This is the answer for score_menu

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,7 @@
 PASSWORD_LENGTH =10
 
 password = input("Enter password: ")
-"""Make use the password is more then 10 length"""
+"""Make sure the password is more then 10 length"""
 while len(password) <= PASSWORD_LENGTH:
     print("Invalid Password")
     password = input("Enter password: ")
