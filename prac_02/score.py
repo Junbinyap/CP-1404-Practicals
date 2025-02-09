540fe89682fd610100eb83036fed3662d39a1c38
Junbin <junbin.yap@my.jcu.edu.au>
2025-02-09 20:42:51 +0800

    This is the answer for score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,16 +1,19 @@
 import random
 
 def main():
+    """main function"""
     score = float(input("Enter score: "))
     result = evaluate_score(score)
     print(result)
 
+    """The random number"""
     random_score = random.randint(0, 100)
     random_result = evaluate_score(random_score)
     print(f"Random score: {random_score:.2f}, Result: {random_result}")
 
 
 def evaluate_score(score):
+    """Evaluate the score"""
     if score < 0 or score > 100:
         return "Invalid score"
     elif score > 50:
