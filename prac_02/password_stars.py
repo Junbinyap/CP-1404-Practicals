34bfdc9f644425928870e2e7695e0dfa50488b8b
Junbin <junbin.yap@my.jcu.edu.au>
2025-02-09 20:40:29 +0800

    This is the answer for password_stars

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,7 @@
 PASSWORD_LENGTH =10
 
 password = input("Enter password: ")
+"""Make use the password is more then 10 length"""
 while len(password) <= PASSWORD_LENGTH:
     print("Invalid Password")
     password = input("Enter password: ")
