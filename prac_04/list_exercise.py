0245ceb0274d6efeb8e78f2a584f5f8ced9d3db6
Junbin <junbin.yap@my.jcu.edu.au>
2025-02-21 17:42:27 +0800

    This is my answer for list_exercise.py

--- prac_04/list_exercise.py
+++ prac_04/list_exercise.py
@@ -10,4 +10,14 @@ def main():
     print("The sum of the numbers is:", sum(numbers))
     print("The average of the numbers is:", sum(numbers) / len(numbers))
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+user_input = input("Please enter your username: ")
+if user_input in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
+
 main()
