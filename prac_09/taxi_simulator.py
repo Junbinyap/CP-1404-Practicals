e5b427aeb901c9fde0d39ce5612582c4154e3f35
Junbin <junbin.yap@my.jcu.edu.au>
2025-04-07 00:17:03 +0800

    i added the taxi simulation with menu-driven interface which allow user to choose a taxi and drive a set distance, calculate and display fare for each trip, track and display total bill. Final, display updated taxi status at the end

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,53 @@
+from prac_06.car import Car
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+def main():
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi= ""
+    print("Let's drive!")
+    print(MENU)
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            display_taxis(taxis)
+            try:
+                taxi_choice = int(input("Choose taxi: "))
+                if 0 <= taxi_choice < len(taxis):
+                    current_taxi = taxis[taxi_choice]
+                else:
+                    print("Invalid taxi choice")
+            except ValueError:
+                print("Invalid input; please enter a number.")
+
+        elif menu_choice == 'd':
+            if current_taxi:
+                current_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far? "))
+                current_taxi.drive(distance_to_drive)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+                total_bill += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
+        else:
+            print("Invalid option")
+        print(f"Bill to date: ${total_bill:.2f}")
+        print(MENU)
+        menu_choice = input(">>> ").lower()
+
+    print(f"Total trip cost: ${total_bill:.2f}")
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
+    """Display numbered list of taxis."""
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+main()
